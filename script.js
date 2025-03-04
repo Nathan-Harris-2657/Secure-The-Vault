0894e4016714ac68363f9558a3cf958342b4161d
Nathan Harris <singdubharris@gmail.com>
2025-03-03 21:06:16 -0500

    Script-For-Vault

---
+++ script.js
@@ -0,0 +1,12 @@
+// Message that will pop up when you are alerted 
+
+ let message = "You have recevied this messeage because you have been chosen to open an important vault"
+
+ //values for code that will be prompted on the alert tab
+ let code1 = 5 * 2
+ let code2 = 30 + 10
+ let code3 = 43 - 4
+
+//  Message with codes on the alert tab
+ alert(`${message}: ${code1} - ${code2} - ${code3}`)
+
